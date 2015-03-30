2025ca1277ee2d53ce96fc61de625b7ba4962a88
lacarmen <carmen.la@mail.utoronto.ca>
2015-03-29 22:14:18 -0400

    Updated js for /de prefix

--- de/js/main.js
+++ de/js/main.js
@@ -5,7 +5,7 @@ $("#menu").children().each(
         var location = window.location.pathname;
         $(this).children().first().removeClass("selected");
 
-        if (location == "/" || location == "/index.html") {
+        if (location == "/de" || location == "/de/index.html") {
             $("#home-link").addClass("selected");
         }
         else if (location.indexOf("docs") > -1) {
